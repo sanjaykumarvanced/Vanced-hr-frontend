46489b5529ad3ff88a2eaf797269f4ef6bc1acd0
Sanjay Kumar <SanjayKumar@vancedsolutions.com>
2023-11-29 11:17:47 +0530

    app new api url

--- src/components/consts/api-url.const.ts
+++ src/components/consts/api-url.const.ts
@@ -1,2 +1,2 @@
-export const apiBaseUrl = "https://api-greenwave-eight.vercel.app/";
+export const apiBaseUrl = "https://vanced-hr-api.vercel.app/";
 // export const apiBaseUrl = "http://localhost:9090";
