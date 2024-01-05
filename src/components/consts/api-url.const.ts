a35b3f8fe0b929b3d801a3390f54d1eec842aaae
sanjaykumar@vancedsolutions.com <SanjayKumar@vancedsolutions.com>
2024-01-05 12:08:44 +0530

    Update api-url.const.ts

--- src/components/consts/api-url.const.ts
+++ src/components/consts/api-url.const.ts
@@ -1,2 +1,2 @@
-// export const apiBaseUrl = "https://vanced-hr-api.vercel.app/";
-export const apiBaseUrl = "http://localhost:9090";
+export const apiBaseUrl = "https://vanced-hr-api.vercel.app/";
+//export const apiBaseUrl = "http://localhost:9090";
